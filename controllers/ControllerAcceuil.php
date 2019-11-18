1272daf1540d1ff84494f76a26e67852d9345a97
Quentin Fontaine <21800691@etu.unicaen.fr>
2019-11-18 13:40:15 +0100

    puput

--- controllers/ControllerAcceuil.php
+++ controllers/ControllerAcceuil.php
@@ -3,4 +3,22 @@
 class ControllerAcceuil
 {
 
+    private $_acceuilManager;
+    private $_view;
+
+    public function __constructor($url){
+
+        if (isset($url) && count($url)>1){
+            throw new Exception('Page introuvable');
+        }
+        else
+        {
+            $this->acceuil();
+        }
+    }
+
+    private function acceuil(){
+        $this->_acceuilManager = new acceuilManager();
+    }
+
 }
--- controllers/ControllerSyllabus.php
+++ controllers/ControllerSyllabus.php
@@ -9,7 +9,7 @@ class ControllerSyllabus
     public function __constructor($url){
 
         if (isset($url) && count($url)>1){
-
+            throw new Exception('Page introuvable');
         }
     }
 
